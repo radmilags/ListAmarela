d0f2b4ff252df4ba3737aa6c2874a585b3de7bd6
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-01-23 15:17:24 -0300

    Update pessoa.cs
    adicao de get dados e metodo ToString

--- Codigo/pessoa.cs
+++ Codigo/pessoa.cs
@@ -1,5 +1,4 @@
 using System;
-
 class Pessoa{
   private string nome;
   private string sobrenome; 
@@ -25,54 +24,19 @@ class Pessoa{
     if(telefone!="") this.telefone = telefone;
     if(senha!="") this.senha = senha;
   }
-  
-  // Criar metodos get para receber o valor do atributos
-  
-  
-  /*class Pessoa
-  {
-    private int id;
-    private string nome;
-    private string sobrenome;
-    private DateTime dataNascimento;
-    private string cpf;
-    private string email;
-    private string sexo;
-    private string endereco;
-    private string bairro;
-    private string cidade;
-    private string telefone;
-    private string senha;
+  public string GetNome(){return this.nome;}
+  public string GetSobrenome(){return this.sobrenome;}
+  public DateTime GetdataNascimento(){return this.dataNascimento;}
+  public string Getcpf(){return this.cpf;}
+  public string Getemail(){return this.email;}
+  public string Getsexo(){return this.sexo;}
+  public string Getendereco(){return this.endereco;}
+  public string Getbairro(){return this.bairro;}
+  public string Getcidade(){return this.cidade;}
+  public string Gettelefone(){return this.telefone;}
+  public string Getsenha(){return this.senha;}
 
-    // Construtor
-    public Pessoa(int id, string nome, string sobrenome, DateTime dataNascimento, string cpf, string email, string sexo, string endereco, string bairro, string cidade, string telefone, string senha)
-    {
-      this.id = id;
-      this.nome = nome;
-      this.sobrenome = sobrenome;
-      this.dataNascimento = dataNascimento;
-      this.cpf = cpf;
-      this.email = email;
-      this.sexo = sexo;
-      this.endereco = endereco;
-      this.bairro = bairro;
-      this.cidade = cidade;
-      this.telefone = telefone;
-      this.senha = senha;
-    }
+  //ToString
 
-    // Métodos de acesso get e set para cada atributo
-    public int Id { get => id; set => id = value; }
-    public string Nome { get => nome; set => nome = value; }
-    public string Sobrenome { get => sobrenome; set => sobrenome = value; }
-    public DateTime DataNascimento { get => dataNascimento; set => dataNascimento = value; }
-    public string Cpf { get => cpf; set => cpf = value; }
-    public string Email { get => email; set => email = value; }
-    public string Sexo { get => sexo; set => sexo = value; }
-    public string Endereco { get => endereco; set => endereco = value; }
-    public string Bairro { get => bairro; set => bairro = value; }
-    public string Cidade { get => cidade; set => cidade = value; }
-    public string Telefone { get => telefone; set => telefone = value; }
-    public string Senha { get => senha; set => senha = value; }
-  }*/
+  public override string ToString(){return $"{this.nome} {this.sobrenome} - {this.telefone}";}
 }
