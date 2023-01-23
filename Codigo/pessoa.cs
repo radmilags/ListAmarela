07945219e8dd9d6f25dd783d16a81a884a5553a3
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-01-23 15:09:21 -0300

    Update pessoa.cs

--- Codigo/pessoa.cs
+++ Codigo/pessoa.cs
@@ -25,4 +25,54 @@ class Pessoa{
     if(telefone!="") this.telefone = telefone;
     if(senha!="") this.senha = senha;
   }
+  
+  // Criar metodos get para receber o valor do atributos
+  
+  
+  /*class Pessoa
+  {
+    private int id;
+    private string nome;
+    private string sobrenome;
+    private DateTime dataNascimento;
+    private string cpf;
+    private string email;
+    private string sexo;
+    private string endereco;
+    private string bairro;
+    private string cidade;
+    private string telefone;
+    private string senha;
+
+    // Construtor
+    public Pessoa(int id, string nome, string sobrenome, DateTime dataNascimento, string cpf, string email, string sexo, string endereco, string bairro, string cidade, string telefone, string senha)
+    {
+      this.id = id;
+      this.nome = nome;
+      this.sobrenome = sobrenome;
+      this.dataNascimento = dataNascimento;
+      this.cpf = cpf;
+      this.email = email;
+      this.sexo = sexo;
+      this.endereco = endereco;
+      this.bairro = bairro;
+      this.cidade = cidade;
+      this.telefone = telefone;
+      this.senha = senha;
+    }
+
+    // Métodos de acesso get e set para cada atributo
+    public int Id { get => id; set => id = value; }
+    public string Nome { get => nome; set => nome = value; }
+    public string Sobrenome { get => sobrenome; set => sobrenome = value; }
+    public DateTime DataNascimento { get => dataNascimento; set => dataNascimento = value; }
+    public string Cpf { get => cpf; set => cpf = value; }
+    public string Email { get => email; set => email = value; }
+    public string Sexo { get => sexo; set => sexo = value; }
+    public string Endereco { get => endereco; set => endereco = value; }
+    public string Bairro { get => bairro; set => bairro = value; }
+    public string Cidade { get => cidade; set => cidade = value; }
+    public string Telefone { get => telefone; set => telefone = value; }
+    public string Senha { get => senha; set => senha = value; }
+  }*/
 }
