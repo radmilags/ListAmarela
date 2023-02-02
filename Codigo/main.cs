bc551a6e14285e007f7353369d3ad03fe7ba9646
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-02-02 11:15:58 -0300

    Update main.cs

--- Codigo/main.cs
+++ Codigo/main.cs
@@ -10,6 +10,7 @@ class Program
     Console.WriteLine("");
     Cliente[] c = new Cliente[3];
     Categoria[] categor = new Categoria[4];
+    Prestador[] prest = new Prestador[3];
     int op = -1;
     while (op != 0)
     {
@@ -18,7 +19,7 @@ class Program
       Console.WriteLine("[1] Visitar sistema");
       Console.WriteLine("[2] Mostrar dados");
       Console.WriteLine("[3] Cadastrar cliente");
-      Console.WriteLine("[4] Cadastrar prestador");
+      Console.WriteLine("[4] Cadastrar Prestador");
       Console.WriteLine("[5] Cadastrar categoria");
       Console.WriteLine("");
     
@@ -43,11 +44,23 @@ class Program
           Console.WriteLine("");
           Console.WriteLine("CADASTRO");
           Console.WriteLine("");
-          c[idcliente] = new Cliente(01, "radmila", "gama","123456789", "@gmail.com", "F", "Rua", "Bairro", "cidade", "87996", "senha123");
+          //id, nome, sobrenome, cpf, email, sexo,  endereco, bairro, cidade, telefone, Categoria, senha
+          c[idcliente] = new Cliente(idcliente+1, "radmila", "gama","123456789", "@gmail.com", "F", "Rua", "Bairro", "cidade", "87996", "senha123");
           idcliente++;
           break;
         }
         case 4:{
+          Console.WriteLine("CADASTRO");
+          Console.WriteLine("");
+          Console.WriteLine("Escolha o número categoria");
+          for(int i = 0; i < 4; i++){
+            Console.WriteLine(categor[i].ToString());
+          }
+          int escolha = int.Parse(Console.ReadLine());
+          prest[idprestador] = new Prestador(01, "radmila", "gama","123456789", "@gmail.com", "F", "Rua", "Bairro", "cidade", "87996", categor[escolha], "senha123");
+          break;
+        }
+        case 5:{
           Console.WriteLine("CADASTRAR CATEGORIA");
           Console.WriteLine("");
           Console.WriteLine("Digite o nome da categoria");
