ae57e6405e16279df064ecc14c59d742c44b4227
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-02-02 10:57:29 -0300

    Create servicos.cs

---
+++ Codigo/servicos.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Servico{
+  private int id;
+  private string nome;
+  private double preco; // preco é por hora, tem que ter um metodo que calcule o preco total
+  private string descricao;
+  private Categoria categor;
+
+  public Servico(int id, string nome, double preco, string descricao, Categoria categor){
+    this.id = id;
+    if(nome !="") this.nome = nome;
+    if(preco > 0) this.preco = preco;
+    this.descricao = descricao;
+    this.categor = categor;
+  }
+}
