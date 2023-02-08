549a9193a1928703a3eea11d7e5ff9024a2b68f6
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2023-02-08 14:28:18 -0300

    Correcoes categorias
    categorias add ok

--- Codigo/main.cs
+++ Codigo/main.cs
@@ -5,7 +5,7 @@ class Program
 {
   public static void Main()
   {
-    int idcliente = 0; int idcategor = 3; int idprestador = 0; int op = -1;
+    int idcliente = 0; int idcategor = 0; int idprestador = 0; int op = -1;
     Console.WriteLine("ListAmarela\n\n");
     
     List<Cliente> c  = new List<Cliente>();
@@ -28,17 +28,13 @@ class Program
         case 0: break;
         case 1:
         {
+          Console.Clear();
           Console.WriteLine("telefone = 400028922");
-          for (int i = 0; i < idcategor; i++)
-          {
-            Console.WriteLine(categor[i].ToString());
-          }
-          Console.WriteLine("");
-          
+          for (int i = 0; i < categor.Count; i++) Console.WriteLine(categor[i].ToString());
           break;
         }
         case 2:
-          Console.WriteLine("");
+          Console.Clear();
           Console.Write("Cliente (1) ou prestador (2) : ");
           int x = int.Parse(Console.ReadLine());
           if(x == 1){
@@ -59,21 +55,18 @@ class Program
         break;
         case 3:
         {
+          Console.Clear();
           Console.WriteLine("CADASTRO\n\n");
           //id, nome, sobrenome, cpf, email, sexo,  endereco, bairro, cidade, telefone, Categoria, senha
           c.Add(new Cliente(idcliente, "radmila", "gama", "123456789", "@gmail.com", "F", "Rua", "Bairro", "cidade", "87996", "senha123"));
           idcliente++;
-          Console.WriteLine("");
-          
           break;
         }
         case 4:
         {
-          Console.WriteLine("");
+          Console.Clear();
           Console.WriteLine("CADASTRO");
-          Console.WriteLine("");
-          Console.WriteLine("Escolha o número categoria");
-          Console.WriteLine("");
+          Console.WriteLine("Escolha o número categoria\n\n");
           
           for (int i = 0; i < 3; i++)
           {
@@ -89,6 +82,7 @@ class Program
         }
         case 5:
         {
+          Console.Clear();
           Console.WriteLine("CADASTRAR CATEGORIA\n\n");
           Console.WriteLine("Digite o nome da categoria");
           categor.Add(new Categoria(idcategor, Console.ReadLine()));
