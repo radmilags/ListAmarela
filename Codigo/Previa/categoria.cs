7ca6dce7300b47a2952620677c2b3053a58f0226
yurithairony <102562875+yurithairony@users.noreply.github.com>
2023-02-11 18:47:49 -0300

    update categoria

--- Codigo/Previa/categoria.cs
+++ Codigo/Previa/categoria.cs
@@ -1,11 +1,16 @@
 using System; // IComparable
 
-class Categoria{
-  private int id;
-  private string nome;
-  public Categoria(int id, string nome) {
-    this.id = id;
-    if(nome!="") this.nome = nome;
-  }
-  public override string ToString(){ return $"[{this.id}] {this.nome}"; }
+
+[Serializable]
+public class Categoria
+{
+    public int id;
+    public string nome;
+    public Categoria () {}
+    public Categoria(int id, string nome)
+    {
+        this.id = id;
+        if (nome != "") this.nome = nome;
+    }
+    public override string ToString() { return $"[{this.id}] {this.nome}"; }
 }
