3c13c8502d2a7fd2af2f95ab56447259a31adbe9
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2025-12-26 12:03:33 +0000

    feat: implement ClienteService logic

---
+++ src/Services/ClienteService.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListAmarela.Services 
+{
+    public class ClienteService 
+    {
+        private List<Cliente> clientes;
+        private Persistencia<Cliente> banco;
+        private string caminhoArquivo = "./clientes.xml";
+
+        public ClienteService() 
+        {
+            this.banco = new Persistencia<Cliente>();
+            this.clientes = banco.AbrirArquivo(caminhoArquivo);
+        }
+
+        public void Cadastrar(Cliente novo) 
+        {
+            this.clientes.Add(novo);
+            this.banco.SalvarArquivo(caminhoArquivo, this.clientes);
+        }
+
+        public List<Cliente> Listar() 
+        {
+            return this.clientes;
+        }
+
+        public int GerarId() 
+        {
+            if (clientes.Count == 0) return 1;
+            return clientes.Max(c => c.Id) + 1;
+        }
+    }
+}
